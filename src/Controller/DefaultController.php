504a558a261c1a383dac7391a046c09c71d78b79
Denis Kornei <mardoksp@mail.ru>
2021-10-21 20:07:14 +0300

    add product page

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,17 +2,44 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", methods="GET", name="homepage")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $entityManager = $this->getDoctrine()->getManager();
+        $productList = $entityManager->getRepository(Product::class)->findAll();
+        dd($productList);
+
         return $this->render('main/default/index.html.twig', []);
+    }
+
+    /**
+     * @Route("/product-add", methods="GET", name="product_add")
+     */
+
+    public function productAdd(Request $request): Response
+    {
+        $product = new Product();
+        $product->setTitle('Product' . rand(1, 100));
+        $product->setDescription('smth');
+        $product->setPrice(10);
+        $product->setQuiantity(1);
+        $product->setCreatedAt(new \DateTimeImmutable());
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($product);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('homepage');
     }
 }
