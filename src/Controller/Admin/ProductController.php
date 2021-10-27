ebbf4b7d9247e42521de113151aa52cd097df05c
Denis Kornei <mardoksp@mail.ru>
2021-10-27 17:26:48 +0300

    for mac upload files

---
+++ src/Controller/Admin/ProductController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Product;
+use App\Form\EditProductFormType;
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin/product", name="admin_product_")
+ */
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/list", name="list")
+     */
+    public function list(ProductRepository $productRepository): Response
+    {
+        $products = $productRepository->findBy(['isDeleted' => false], ['id' => 'ASC'], 50);
+
+        return $this->render('admin/product/list.html.twig', [
+            'products' => $products
+        ]);
+    }
+
+    /**
+     * @Route("/edit/{id}", name="edit")
+     * @Route("/add", name="add")
+     */
+    public function edit(Request $request, Product $product = null): Response
+    {
+       $form = $this->createForm(EditProductFormType::class, $product);
+       $form->handleRequest($request);
+
+       if ($form->isSubmitted() && $form->isValid()) {
+           $entityManager = $this->getDoctrine()->getManager();
+           $entityManager->flush();
+
+           return $this->redirectToRoute('admin_product_edit', ['id' => $product->getId()]);
+       }
+
+       return $this->render('admin/product/edit.html.twig', [
+           'product' => $product,
+           'form' => $form->createView()
+       ]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     */
+    public function delete(): Response
+    {
+        //
+    }
+
+}
