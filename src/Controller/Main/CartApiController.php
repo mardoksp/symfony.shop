c6f3812662b26f772865da105b22f4097616764f
Денис Корней <deniskornei@MacBook-Air.local>
2021-11-08 03:03:41 +0300

    Add cart and add in product cart

--- src/Controller/Main/CartApiController.php
+++ src/Controller/Main/CartApiController.php
@@ -4,6 +4,8 @@ namespace App\Controller\Main;
 
 use App\Entity\Cart;
 use App\Entity\CartProduct;
+use App\Repository\CartProductRepository;
+use App\Repository\CartRepository;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -20,34 +22,43 @@ class CartApiController extends AbstractController
     /**
      * @Route("/cart", methods="POST",  name="cart_save")
      */
-    public function saveCart(Request $request, ProductRepository $productRepository): Response
+    public function saveCart(Request $request, CartRepository $cartRepository, CartProductRepository $cartProductRepository, ProductRepository $productRepository): Response
     {
         $productId = $request->request->get('productId');
         $phpSessionId = $request->cookies->get('PHPSESSID');
 
         $product = $productRepository->findOneBy(['uuid' =>$productId]);
 
-        $cart = new Cart();
-        $cart->setSessionId($phpSessionId);
+        $cart = $cartRepository->findOneBy(['sessionId' => $phpSessionId]);
 
-        $cartProduct = new CartProduct();
-        $cartProduct->setCart($cart);
-        $cartProduct->setQuantity(1);
-        $cartProduct->setProduct($product);
+        if (!$cart) {
+            $cart = new Cart();
+            $cart->setSessionId($phpSessionId);
+        }
 
-        $cart->addCartProduct($cartProduct);
+        $cartProduct = $cartProductRepository->findOneBy(['cart' => $cart, 'product' => $product]);
+        if (!$cartProduct) {
+            $cartProduct = new CartProduct();
+            $cartProduct->setCart($cart);
+            $cartProduct->setQuantity(1);
+            $cartProduct->setProduct($product);
+
+            $cart->addCartProduct($cartProduct);
+        } else {
+            $quantity = $cartProduct->getQuantity() + 1;
+            $cartProduct->setQuantity($quantity);
+        }
 
         $entityManager = $this->getDoctrine()->getManager();
         $entityManager->persist($cart);
         $entityManager->persist($cartProduct);
         $entityManager->flush();
-        dd($cart, $cartProduct);
 
-       return new JsonResponse([
-           'success' => false,
-           'data' => [
-               'test' => 123
-           ]
-       ]);
+        return new JsonResponse([
+            'success' => false,
+            'data' => [
+                'test' => 123
+            ]
+        ]);
     }
 }
